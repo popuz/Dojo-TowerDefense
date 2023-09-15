d9024c8598069a803b13a5a37978dcbbf787bc2d
Vitaly Popuzin <popuzin@gmail.com>
2023-09-15 13:56:12 +0200

    brought service locator

---
+++ Assets/Scripts/Infrastructure/ServicesExample.cs
@@ -0,0 +1,23 @@
+﻿namespace Infrastructure
+{
+    public class ServicesExample
+    {
+        // 1. Define your service interface
+        public interface INewService: IService{}
+        
+        // 2. Implement your service interface
+        public class NewService: INewService{}
+
+        // 3. Register your service in the AllService Container
+        private void ServiceRegistrationExample()
+        {
+            AllServices.Container.RegisterSingle<INewService>(new NewService());
+        }
+        
+        // 4. Get your service from the AllService Container
+        private void GettingServiceExample()
+        {
+            var newService = AllServices.Container.Single<INewService>();
+        }
+    }
+}
